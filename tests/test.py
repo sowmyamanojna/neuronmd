3994d02214c3cc4996261324cfe9238e34e29f1c
sowmyamanojna <sowmyamanojna@gmail.com>
2021-08-23 20:03:38 +0530

    Changed the images

--- tests/test.py
+++ tests/test.py
@@ -5,38 +5,38 @@ plt.rcParams['font.serif'] = "Cambria"
 plt.rcParams['font.family'] = "serif"
 
 #######################################
-# # Initialize a HH Neuron Class
-# hhneuron = HHNeuron()
+# Initialize a HH Neuron Class
+hhneuron = HHNeuron()
 
-# # tmax - Max time (ms) to simulate the neuron
-# # dt - time step of simulation
-# # I - Current for simulating the neuron
+# tmax - Max time (ms) to simulate the neuron
+# dt - time step of simulation
+# I - Current for simulating the neuron
 
-# tmax = 100
-# dt = 0.01
-# I = 0.1
-# t = np.arange(0, tmax, dt)
+tmax = 100
+dt = 0.01
+I = 0.1
+t = np.arange(0, tmax, dt)
 
-# hhneuron.simulate(t, I)
-# hhneuron.plot()
+hhneuron.simulate(t, I)
+hhneuron.plot()
 
-# current_list = np.arange(0.01, 0.5, 0.01)
-# hhneuron.animate(t, ylim=[-85,40], current_list=current_list)
-# hhneuron.animate(t, current_list=current_list, name="no_ylim")
+current_list = np.arange(0.01, 0.5, 0.01)
+hhneuron.animate(t, ylim=[-85,40], current_list=current_list)
+hhneuron.animate(t, current_list=current_list, name="no_ylim")
 
-# #######################################
-# izhneuron = IzhNeuron()
+#######################################
+izhneuron = IzhNeuron()
 
-# tmax = 1000
-# dt = 0.01
-# t = np.arange(0, tmax, dt)
-# I = 5
-# izhneuron.simulate(t, I)
-# izhneuron.plot()
+tmax = 1000
+dt = 0.01
+t = np.arange(0, tmax, dt)
+I = 5
+izhneuron.simulate(t, I)
+izhneuron.plot()
 
-# current_list = np.arange(0.01, 10, 0.5)
-# izhneuron.animate(t, ylim=[-80,35], current_list=current_list)
-# izhneuron.animate(t, current_list=current_list, name="no_ylim")
+current_list = np.arange(0.01, 10, 0.5)
+izhneuron.animate(t, ylim=[-80,35], current_list=current_list)
+izhneuron.animate(t, current_list=current_list, name="no_ylim")
 
 #######################################
 fhnneuron = FHNNeuron()
